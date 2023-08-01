838ad2512d9320b1fab580f45b00d97f7f9f717c
Reyy344 <141038093+Reyy344@users.noreply.github.com>
2023-08-01 08:25:58 +0700

    Add files via upload

---
+++ KalkulatorBI.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class KalkulatorBI {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Selamat datang di Kalkulator Sederhana");
+        System.out.print("Masukkan angka pertama: ");
+        double angka1 = scanner.nextDouble();
+
+        System.out.print("Masukkan operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        System.out.print("Masukkan angka kedua: ");
+        double angka2 = scanner.nextDouble();
+
+        double hasil = 0;
+        switch (operator) {
+            case '+':
+                hasil = angka1 + angka2;
+                break;
+            case '-':
+                hasil = angka1 - angka2;
+                break;
+            case '*':
+                hasil = angka1 * angka2;
+                break;
+            case '/':
+                if (angka2 != 0) {
+                    hasil = angka1 / angka2;
+                } else {
+                    System.out.println("Error: Tidak dapat melakukan pembagian dengan 0");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Error: Operator tidak valid");
+                return;
+        }
+
+        System.out.println("Hasil: " + hasil);
+    }
+}
